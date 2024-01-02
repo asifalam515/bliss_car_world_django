5da73b7f0be2e172991531001cf3974ac82138c7
asifalam515 <asibulalam515@gmail.com>
2024-01-03 00:22:16 +0600

    done

--- bliss_car_world/views.py
+++ bliss_car_world/views.py
@@ -101,28 +101,7 @@ class DetailCarView(DetailView):
                 
 
 
-# class CarDetails(DetailView):
-#     model=CarModel
-#     pk_url_kwarg='car_id'
-#     template_name='view_details.html'
-    
-#     def post(self,request,*args, **kwargs):
-#         comment_form=CommentForm(data=self.request.POST)
-#         car=self.get_object()
-#         if comment_form.is_valid():
-#              new_comment = comment_form.save()
-#              new_comment.post = car
-#              new_comment.save()
-#              return self.get(request,*args, **kwargs)
-    
-#     def get_context_data(self, **kwargs) :
-#         context=super().get_context_data(**kwargs)
-#         car=self.object    
-#         comments =car.comments.all()
-#         comment_form=CommentForm()
-#         context['comments']=comments
-#         context['comment_form']=comment_form
-#         return context
+
         
 
 @login_required
